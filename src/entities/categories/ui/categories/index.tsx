257c45a96d09ac14bd8c0d9dc2de7d0fccf92fcc
Andrey <lionlonn01@inbox.ru>
2024-02-29 20:50:41 +0300

    Transferred the data about the category to the api

--- src/entities/categories/ui/categories/index.tsx
+++ src/entities/categories/ui/categories/index.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import { View, Text, SafeAreaView, FlatList, TouchableOpacity } from "react-native";
 import { categoryStyle } from "./style";
-import { images } from '../../image/index';
 import Arrow from '../../image/arrow.svg'
-
-const { developer, marketer, blogger, photograther, creative } = images;
-
-const DATA = [
-    { image: developer, id: 1, title: "Developer", count: 21 },
-    { image: marketer, id: 2, title: "Marketer", count: 7 },
-    { image: blogger, id: 3, title: "Blogger", count: 12 },
-    { image: photograther, id: 4, title: "Photographer", count: 13 },
-    { image: creative, id: 5, title: "Creative", count: 3 }
-];
+import DATA from '../../api/index'
 
 type ItemProps = {
     title: string,
