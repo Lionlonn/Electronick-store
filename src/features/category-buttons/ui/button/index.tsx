ee225f21537a95b6494df407cbdf547316f7a248
Andrey <lionlonn01@inbox.ru>
2024-04-06 19:00:31 +0300

    fix handleClickCategory buttons

--- src/features/category-buttons/ui/button/index.tsx
+++ src/features/category-buttons/ui/button/index.tsx
@@ -1,41 +1,31 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { Button, Text, TouchableOpacity, View } from "react-native";
-import { useAppDispatch, useStateSelector } from "src/shared/hooks";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useAppDispatch } from "src/shared/hooks";
 import { getButtonDeveloperProduct, getButtonProductsAll, getFilmakingProduct, getPhotographyProduct, getPodcastProduct } from "../../model";
 
+interface ClickHandles {
+    [key: string]: () => void
+}
 
 
 export const CategoryButtons = () => {
     const categories = ['Show all', 'Developer', 'Podcast creator', 'Filmaking', 'Photography']; 
     const dispatch = useAppDispatch() 
 
-
-    
-    const handleClickCategory = (category: string) =>  {
-            switch (category) {
-                case 'Show all':
-                    dispatch(getButtonProductsAll())
-                    break
-                case 'Developer':
-                    dispatch(getButtonDeveloperProduct())
-                    break
-                case 'Podcast creator':
-                    dispatch(getPodcastProduct())
-                    break
-                case 'Filmaking':
-                    dispatch(getFilmakingProduct())
-                    break
-                case 'Photography':
-                    dispatch(getPhotographyProduct())
-            }
+    const handleClickCategory: ClickHandles = {
+        'Show all': () => dispatch(getButtonProductsAll()),
+        'Developer': () => dispatch(getButtonDeveloperProduct()),
+        'Podcast creator': () => dispatch(getPodcastProduct()),
+        'Filmaking': () => dispatch(getFilmakingProduct()),
+        'Photography': () => dispatch(getPhotographyProduct())
     }
 
     return (
-        <View>
+        <View style={styles.container}>
             {categories.map((category, index) => (
                 <TouchableOpacity 
                     key={index} 
-                    onPress={() => handleClickCategory(category)}
+                    onPress={handleClickCategory[category]}
                 >
                     <Text>{category}</Text>
                 </TouchableOpacity>
@@ -43,3 +33,9 @@ export const CategoryButtons = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
