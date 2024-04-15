1443a7311d4ffaf538871f169c571496bd8c2b31
Andrey <lionlonn01@inbox.ru>
2024-04-15 20:39:17 +0300

    styling images swiper widgets

--- src/entities/item-block-images/ui/right-block/index.tsx
+++ src/entities/item-block-images/ui/right-block/index.tsx
@@ -12,9 +12,36 @@ const windowHeight = Dimensions.get('window').height;
 export const RightItemImageBlock = (props: Props) => {
    
    return (
-    <View>
-        
+    <View style={styles.container}>
+        {props.images.map(img => (
+            <View style={styles.wrapper}>
+                <Image
+                    style={styles.image}
+                    source={{
+                        uri: img
+                    }}
+                />
+            </View>
+        ))}
     </View>
    )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        gap: 6
+    },
+    wrapper: {
+        borderWidth: 2,
+        borderColor: '0.6px solid rgb(186, 92, 61)',
+        padding: 5,
+        borderRadius: 8,
+        alignSelf: 'center',
+        backgroundColor: 'white'
+    },
+    image: {
+        width: 60,
+        height: 60,
+        borderRadius: 8
+    }
+})
--- src/widgets/item-images-swiper/ui/index.tsx
+++ src/widgets/item-images-swiper/ui/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import { View } from "react-native";
 import { ImagesSwiper } from "src/entities/item-block-images/ui/center-block";
+import { RightItemImageBlock } from "src/entities/item-block-images/ui/right-block";
 
 interface Props {
     route: any
@@ -9,8 +11,34 @@ interface Props {
 export const ViewItemImagesBlock = (props: Props) => {
     const img = props.route.params.item.img
     return (
-        <View>
-            <ImagesSwiper images={img}/>
+        <View style={styles.container}>
+            <View style={styles.centerImage}>
+                <ImagesSwiper images={img}/>
+
+                <View style={styles.rightImage}>
+                    <RightItemImageBlock images={img}/>
+                </View>
+            </View>
+            
+            
+            
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+          
+    },
+    centerImage: {
+      position: 'relative',
+      alignItems: 'flex-end',
+      justifyContent:"center",
+      marginRight: 24
+    },
+    rightImage: {
+      position: 'absolute'
+    }
+})
