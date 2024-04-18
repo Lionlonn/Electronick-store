68f9b9f1c5cd7ae55155483cccc7da08860ec0ad
Andrey <lionlonn01@inbox.ru>
2024-04-18 20:12:50 +0300

    styling product info header block

--- src/entities/product-info/ui/info/index.tsx
+++ src/entities/product-info/ui/info/index.tsx
@@ -16,12 +16,21 @@ export const ProductInfo = (props: Props) => {
     
     return (
         <View style={styles.container}> 
-            <View>
-                <Text>{props.name}</Text>
-                <View style={styles.subTitleInfo}>
-                    <Text>{props.category}</Text>
-                    <Text>{props.rating}</Text>
-                    <RatingStar rating={props.rating}/> 
+            <View style={styles.wrapper}>
+                <View>
+                    <Text style={styles.titleProduct}>{props.name}</Text>
+                    <View style={styles.subTitleInfo}>
+                        <Text style={styles.textCategory}>{props.category}</Text>
+                        <View style={styles.point}></View>
+                        <Text style={styles.textRating}>{props.rating}</Text>
+                        <View style={styles.ratingImage}>
+                            <RatingStar rating={props.rating}/> 
+                        </View>
+                        
+                    </View> 
+                </View>
+                <View style={styles.rightPrice}>
+                    <Text style={styles.textPrice}>${props.price}</Text>
                 </View>
             </View>
         </View>
@@ -30,13 +39,68 @@ export const ProductInfo = (props: Props) => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        margin: 20,
+    },
+    wrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    titleProduct: {
+        fontFamily: 'Avenir-Heavy',
+        fontSize: 21,
+        fontWeight: '900',
+        lineHeight: 29,
+        color: 'black',
+        marginBottom: 8
     },
     subTitleInfo: {
         flexDirection: 'row',
-        gap: 10
+        gap: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
     },
-    fullStar: {
-        backgroundColor: 'rgb(242, 201, 76)'
+    textCategory: {
+        fontSize: 14,
+        fontFamily: 'Avenir-Heavy',
+        fontWeight: '500',
+        lineHeight: 16, 
+        color: 'rgb(166, 167, 152)'
+
+
+    },
+    point: {
+        width: 3,
+        height: 3,
+        borderRadius: 22,
+        backgroundColor: 'rgb(166, 167, 152)',
+        top: -1
+    },
+    textRating: {
+        color: 'rgb(27, 61, 47)',
+        fontSize: 14,
+        fontFamily: 'Avenir-Heavy',
+        fontWeight: '500',
+        lineHeight: 16, 
+    },
+    ratingImage: {
+        top: -2
+    },
+    rightPrice: {
+        backgroundColor: 'rgb(245, 250, 248)',
+        width: 'auto',
+        height: 'auto',
+        borderRadius: 8
+    },
+    textPrice: {
+        padding: 20,
+        color: 'rgb(27, 61, 47)',
+        fontFamily: 'Avenir-Heavy',
+        fontSize: 21,
+        fontWeight: '900',
+        lineHeight: 29,
     }
+
 })
