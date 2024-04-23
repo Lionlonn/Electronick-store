114a5309e31505aaf1a40c257ada7fd1d0ccb840
Andrey <lionlonn01@inbox.ru>
2024-04-23 13:10:28 +0300

    fix total price and add styling text

--- src/features/total-price/ui/index.tsx
+++ src/features/total-price/ui/index.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, useWindowDimensions } from "react-native";
 import { useStateSelector } from "src/shared/hooks";
 
 
 export const CounterTotalPrice = () => {
     const cart = useStateSelector(state => state.cartSlice);
-    
-    const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+    const defaultSize = 16;
+    const width = useWindowDimensions().width;
+    const totalPrice = (cart.reduce((acc, item) => acc + item.price * item.quantity, 0)).toFixed(2);
     
     return (
             <View style={styles.totalPrice}>
-                <Text>Total</Text>
-                <Text>{totalPrice}</Text>
+                <Text style={[
+                    styles.textTotal,
+                    {fontSize: width > 420 ? defaultSize * 1.4 : defaultSize}
+                    ]}>Total</Text>
+                <Text style={[
+                    styles.textPrice,
+                    {fontSize: width > 420 ? defaultSize * 1.4 : defaultSize}
+                ]}>${totalPrice}</Text>
             </View>
     )
 }
@@ -23,6 +30,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         width: '100%'
+    },
+    textTotal: {
+        color: 'black',
+        fontFamily: 'Avenir-Black',
+        fontWeight: '800'
+    },
+    textPrice: {
+        color: 'rgb(186, 92, 61)',
+        fontWeight: '800',
+        fontFamily: 'Avenir-Black'
     }
     
 })
