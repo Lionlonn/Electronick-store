49b784c96ba91b4b365ab53f33cff5d36b9118ed
Andrey <lionlonn01@inbox.ru>
2024-06-02 19:37:03 +0300

    add use state user

--- src/pages/basket/ui/index.tsx
+++ src/pages/basket/ui/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, TouchableOpacity, useWindowDimensions } from "react-native";
 import { ScrollView, StyleSheet, View } from "react-native";
 import { Cart } from "src/entities/product/ui/cart-view";
 import { ActionButtonsProduct } from "src/features/action-button";
@@ -8,14 +8,31 @@ import { useStateSelector } from "src/shared/hooks";
 import { useStateUserAuth } from "features/auth/firebase/state-user";
 
 
-export const BasketPage = () => {
+
+
+export const BasketPage = ({navigation}: any) => {
+    const width = useWindowDimensions().width
     const cart = useStateSelector(state => state.cartSlice);
     const { initializing, user } = useStateUserAuth();
 
-    if (initializing) return <Text>Loading...</Text>
+    const fontSize = width > 420 ? 22 : 16
 
-    if (!user) return <Text>Please auth user</Text>
+    if (initializing) return <Text>Loading...</Text>;
 
+    if (!user) {
+        return (
+            <View style={styles.stateUser}>
+                <Text style={[styles.stateUserText, {fontSize: fontSize}]}>Please login user</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('LoginPage')}>    
+                    <Text style={[styles.stateUserLogin, {fontSize: fontSize}]}>Login</Text>
+                </TouchableOpacity>
+            </View>
+            
+        );
+    }
+
+
+   
 
     return (
         <ScrollView 
@@ -51,6 +68,21 @@ export const BasketPage = () => {
 
 
 const styles = StyleSheet.create({
+    stateUser: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 20
+    },
+    stateUserText: {
+        fontFamily: 'Avenir-Heavy',
+        fontWeight: '900'
+    },
+    stateUserLogin: {
+        fontFamily: 'Avenir-Heavy',
+        fontWeight: '900',
+        color: 'rgb(186, 92, 61)'
+    },
     container: {
         backgroundColor: '#FFF',
         flexGrow: 1,
